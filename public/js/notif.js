f6fd6e86ef2176131850c557da6d08e4e49ed84d
alesahe <confuzzlementation@gmail.com>
2024-12-18 21:26:59 +1100

    Update notif.js
    to allow the notifications to be shown based on event date

--- public/js/notif.js
+++ public/js/notif.js
@@ -1,10 +1,15 @@
 // define global variables yay
 const notifBtn = document.getElementById("enableNotifs");
 const d  = new Date();
+let lastSent;
+let currDate = d.getDate();
+let currMonth = d.getMonth();
 
 // when notifBtn is clicked, make the popup at the top work
 function askNotifPermission() {
     function handlePermission(permission) {
+        const msg = document.querySelector(".msg");
+        
         // browser stores information regardless
         if (!Reflect.has(Notification, "permission")) {
             Notification.permission = permission;
@@ -12,9 +17,8 @@ function askNotifPermission() {
 
         // Set the button to shown or hidden, depending on what the user answers
         if (Notification.permission === "denied" || Notification.permission === "default") {
-            const msg = document.querySelector(".msg");
             msg.style.display = "block";
-            msg.textContent = "Your notifications are turned off. \n Click on the button below to enable notifications.";
+            msg.textContent = "Your notifications are turned off.";
         } else {
             msg.textContent = "You have already enabled notifications."
             notifBtn.style.display = "none";
@@ -44,7 +48,25 @@ function checkNotifPromise() {
 
 notifBtn.addEventListener("click", askNotifPermission);
 
-(async () => {
+// converts numerical date to words as stored in the database
+function convertDate (date){
+    months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"];
+    return months[date];
+}
+
+// asynchronous functions
+(async () => {    
+        // // show an error message
+    // check notification permission
+    let granted = false;
+
+    if (Notification.permission === "granted") {
+        granted = true;
+    } else if (Notification.permission !== "denied") {
+        let permission = await Notification.requestPermission();
+        granted = permission === "granted" ? true : false;
+    }
+
     // create and show the notification
     const showNotification = (title) => {
         const newNotif = new Notification(`${title}`, {
@@ -64,33 +86,26 @@ notifBtn.addEventListener("click", askNotifPermission);
         });
     }
 
-    // // show an error message
-    // const showError = () => {
-    //     const error = document.querySelector(".error");
-    //     error.style.display = "block";
-    //     error.textContent = "Your notifications are turned off. \n Click on the button below to enable notifications.";
-    // }
-
-    // check notification permission
-    let granted = false;
-
-    if (Notification.permission === "granted") {
-        granted = true;
-    } else if (Notification.permission !== "denied") {
-        let permission = await Notification.requestPermission();
-        granted = permission === "granted" ? true : false;
-    }
-
-    // show notification or error
-    if (d.getDate()==13 && d.getMonth()==11){ // make it so that it checks if is the same as in database
-        if (granted) {
-            showNotification("meteor shower");
-            notifHeading.style.display="none";
-            notifBtn.style.display = "none";
-        } 
-        // else showError();
-    } else {
-        console.log(d.getDate());
-        console.log(d.getMonth());
+    // post request for dates of things
+    if (lastSent!=currDate){
+        lastSent=currDate;
+        await fetch("/notifDates", {
+            method: "POST",
+            body: JSON.stringify ({
+                cDate: currDate,
+                cMonth: convertDate(currMonth)
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+        .then(async function(response){
+            if(!response.ok){
+                throw new Error("notif response invalid ;-;");
+            };
+            const dates = await response.json();
+            for (let i = 0; i<dates.length; i++) showNotification(dates);
+            return await response;
+        })
     }
 })();
