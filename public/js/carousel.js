4d7ee354f814e9c82bb712ed66a8f42b30fe4107
alesahe <confuzzlementation@gmail.com>
2024-12-30 16:57:12 +1100

    removed dots
    they look bad

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,3 +1,4 @@
+// code from https://www.w3schools.com/howto/howto_js_slideshow.asp, with amendments
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -12,15 +13,10 @@ function currentSlide(n) {
 function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
     if (n > slides.length) {slideIndex = 1}    
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex-1].style.display = "block";
 }
